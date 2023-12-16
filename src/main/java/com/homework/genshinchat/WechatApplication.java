c06400b97e3a83943213d21b81505588d4b65e95
Alan <1563085328@qq.com>
2023-12-16 12:00:08 +0800

    "add"

--- src/main/java/com/homework/genshinchat/WechatApplication.java
+++ src/main/java/com/homework/genshinchat/WechatApplication.java
@@ -28,7 +28,7 @@ public class WechatApplication {
     @Bean
     public RestHighLevelClient client() {
         return new RestHighLevelClient(RestClient.builder(
-                HttpHost.create("http://118.25.143.28:9200")
+                HttpHost.create("http://192.158.231.32:9200")
         ));
     }
 }
