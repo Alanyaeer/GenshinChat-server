a1fddbc513e1ac82dce1e6fdeffeebef8618e093
嘉豪舞团-吴嘉豪 <1563085328@qq.com>
2025-09-09 00:06:04 +0800

    feat: 完成Ping消息回复

--- genshin-chat-server/src/main/java/com/homework/chatserver/netty/handler/WebSocketConnectionMetaHandler.java
+++ genshin-chat-server/src/main/java/com/homework/chatserver/netty/handler/WebSocketConnectionMetaHandler.java
@@ -5,10 +5,13 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.QueryStringDecoder;
+import io.netty.handler.codec.http.websocketx.PingWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.PongWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.timeout.IdleState;
 import io.netty.handler.timeout.IdleStateEvent;
 import io.netty.util.AttributeKey;
+import io.netty.util.ReferenceCountUtil;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
@@ -18,15 +21,24 @@ import static com.homework.common.entity.constants.MessageMetaConstants.MY_ID;
 
 @Slf4j
 public class WebSocketConnectionMetaHandler extends ChannelInboundHandlerAdapter{
-
-
+    /**
+     * 拦截Ping消息
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        if(msg instanceof PingWebSocketFrame){
+            try {
+                ctx.writeAndFlush(new PongWebSocketFrame());
+            } finally {
+                ReferenceCountUtil.release(msg);
+            }
+        }
+    }
 
     @Override
     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
         // 握手成功事件
-        if (evt instanceof WebSocketServerProtocolHandler.HandshakeComplete) {
-            WebSocketServerProtocolHandler.HandshakeComplete complete =
-                    (WebSocketServerProtocolHandler.HandshakeComplete) evt;
+        if (evt instanceof WebSocketServerProtocolHandler.HandshakeComplete complete) {
 
             String uri = complete.requestUri(); // e.g. /v2/im/server?userId=123&token=abc
             QueryStringDecoder decoder = new QueryStringDecoder(uri);
@@ -41,9 +53,9 @@ public class WebSocketConnectionMetaHandler extends ChannelInboundHandlerAdapter
                 Channel channel = ctx.channel();
                 channel.attr(AttributeKey.valueOf(MY_ID)).set(userId);
                 ChannelContext.select().registerChannel(userId, channel);
-                System.out.println("用户 " + userId + " 绑定成功");
+                log.info("用户 {} 绑定成功", userId);
             } else {
-                System.out.println("握手缺少 userId 参数");
+                log.info("握手缺少 userId 参数");
                 ctx.close();
             }
         }
@@ -65,14 +77,14 @@ public class WebSocketConnectionMetaHandler extends ChannelInboundHandlerAdapter
         String userId = ctx.channel().attr(key).get();
         if (userId != null) {
             ChannelContext.select().unRegisterChannel(userId);
-            System.out.println("❌ 用户 " + userId + " 断开连接");
+            log.info("❌ 用户 {} 断开连接", userId);
         }
         super.handlerRemoved(ctx);
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        System.err.println("异常: " + cause.getMessage());
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        log.info("异常: {}", cause.getMessage());
         ctx.close();
     }
 }
