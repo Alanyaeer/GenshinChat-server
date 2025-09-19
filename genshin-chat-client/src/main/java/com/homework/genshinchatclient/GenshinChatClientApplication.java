73e2efad65d62182bfe150262d888278616abd08
wujiahao <wujiahao@coocaa.com>
2025-09-19 17:05:07 +0800

    feat：新增 NettyWebClient 层

---
+++ genshin-chat-client/src/main/java/com/homework/genshinchatclient/GenshinChatClientApplication.java
@@ -0,0 +1,13 @@
+package com.homework.genshinchatclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenshinChatClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenshinChatClientApplication.class, args);
+    }
+
+}
